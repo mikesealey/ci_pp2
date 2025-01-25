61fb59a268b797f013f17c2d6c9bf872b0efdc53
mikesealey <mikesealey@hotmail.com>
2025-01-25 19:57:53 +0000

    adds test for basic resetGame() invocation

--- assets/tests/script.test.js
+++ assets/tests/script.test.js
@@ -4,7 +4,7 @@
 
 const $ = require("jquery");
 const fs = require("fs");
-const path = require("path")
+const path = require("path");
 global.$ = $;
 import {
   selectTile,
@@ -15,27 +15,46 @@ import {
   resetGame,
   generateGrid,
   setAndFetchTopScore,
-} from "../js/script"
+} from "../js/script";
 
 import { getByText } from "@testing-library/dom";
 
-describe("Testing generateGrid()", ()=> {
-  test("Generates a square grid of size 3", ()=> {
+describe("Testing generateGrid()", () => {
+  test("Generates a square grid of size 3", () => {
     // mock a DOM locally
     document.body.innerHTML = '<div id="board"></div>';
     // Invoke
     generateGrid(3);
     // Assert
     // Three Rows
-    expect($("#board").children().length).toBe(3)
+    expect($("#board").children().length).toBe(3);
     // Each row has three columns
     // Get the rows of the board
-    const rows = $("#board").children()
+    const rows = $("#board").children();
     // Loop over them to check they each have 3 child divs
     rows.each((index, row) => {
-      expect($(row).children().length).toBe(3)
-    })
-  })
+      expect($(row).children().length).toBe(3);
+    });
+  });
+
+  // Not currently useful, but testting this functionality will make further developments easier
+  test("Generates a square grid of size 12", () => {
+    // mock a DOM locally
+    document.body.innerHTML = '<div id="board"></div>';
+    // Invoke
+    generateGrid(12);
+    // Assert
+    // Three Rows
+    console.log($("#board"))
+    expect($("#board").children().length).toBe(12);
+    // Each row has three columns
+    // Get the rows of the board
+    const rows = $("#board").children();
+    // Loop over them to check they each have 12 child divs
+    rows.each((index, row) => {
+      expect($(row).children().length).toBe(12);
+    });
+  });
 
   test("Dresses the tiles with the TicTacToe text", () => {
     // mock a DOM locally
@@ -47,5 +66,19 @@ describe("Testing generateGrid()", ()=> {
     expect($("#b2").text()).toBe("Tac");
     expect($("#c3").text()).toBe("Toe");
   });
-})
+});
 
+describe("Testing resetGame()", () => {
+  test("Checks that all tiles have been revered to '?' innerText", () => {
+    // mock a DOM locally
+    document.body.innerHTML = '<div id="board"></div>';
+    // Invoke
+    generateGrid(3);
+    resetGame()
+    // Assert
+    $(".tile").each((index, tile) => {
+      // loops to check every tile
+      expect($(tile).text()).toBe("?")
+    });
+  });
+});
