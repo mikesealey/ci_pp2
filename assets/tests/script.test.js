e0f142d8d2b8f6538ceafe30905de934232ecea0
mikesealey <mikesealey@hotmail.com>
2025-01-25 22:33:01 +0000

    removes unused code

--- assets/tests/script.test.js
+++ assets/tests/script.test.js
@@ -8,13 +8,10 @@ const path = require("path");
 global.$ = $;
 import {
   selectTile,
-  computerSelectTile,
   checkWinStatus,
   checkDrawStatus,
-  playGame,
   resetGame,
   generateGrid,
-  setAndFetchTopScore,
 } from "../js/script";
 
 import { getByText } from "@testing-library/dom";
@@ -44,15 +41,31 @@ describe("Testing generateGrid()", () => {
     // Invoke
     generateGrid(12);
     // Assert
-    // Three Rows
-    console.log($("#board"))
+    // 12 Rows
     expect($("#board").children().length).toBe(12);
-    // Each row has three columns
+    // Each row has 12 columns
     // Get the rows of the board
     const rows = $("#board").children();
     // Loop over them to check they each have 12 child divs
     rows.each((index, row) => {
       expect($(row).children().length).toBe(12);
+    });
+  });
+
+  test("Generates a square grid of size 26", () => {
+    // mock a DOM locally
+    document.body.innerHTML = '<div id="board"></div>';
+    // Invoke
+    generateGrid(26);
+    // Assert
+    // 26 Rows
+    expect($("#board").children().length).toBe(26);
+    // Each row has 26 columns
+    // Get the rows of the board
+    const rows = $("#board").children();
+    // Loop over them to check they each have 26 child divs
+    rows.each((index, row) => {
+      expect($(row).children().length).toBe(26);
     });
   });
 
@@ -81,4 +94,41 @@ describe("Testing resetGame()", () => {
       expect($(tile).text()).toBe("?")
     });
   });
+
+  test("Checks that all tiles have been revered to '?' innerText on a grid of 26*26", () => {
+    // mock a DOM locally
+    document.body.innerHTML = '<div id="board"></div>';
+    // Invoke
+    generateGrid(26);
+    resetGame()
+    // Assert
+    $(".tile").each((index, tile) => {
+      // loops to check every tile
+      expect($(tile).text()).toBe("?")
+    });
+  });
 });
+
+describe("Testing selectTile()", ()=> {
+  test("checks that a player can select a tile", ()=> {
+    // mock a DOM locally
+    document.body.innerHTML = '<div id="board"></div>';
+    // Player object
+    const testPlayer = {
+      name: "Jest",
+      symbol: "X",
+      score: 0,
+      color: "red",
+    };
+    // Invoke
+    generateGrid(3)
+    resetGame()
+
+    const tile = document.getElementById("a1")
+    selectTile(tile, testPlayer)
+    // Assert
+    // inner text of tile should be equal to player.symbol
+    expect(tile.classList.contains(testPlayer.color)).toBe(true)
+  })
+})
+
